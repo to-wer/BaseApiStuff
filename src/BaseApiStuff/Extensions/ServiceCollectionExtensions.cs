e565405bf261f988ab16536b92fd9f8caecf93e8
to-wer <tw@to-wer.dev>
2024-12-03 21:25:42 +0100

    Update ServiceCollectionExtensions to add authentication
    - Update BaseApiStuff.csproj to version 0.2.3
    - Add BaseApiStuff.Services namespace
    - Add Microsoft.AspNetCore.Identity and Microsoft.Extensions.DependencyInjection namespaces
    - Modify ServiceCollectionExtensions to add authentication service
    - Fix class name typo: ServiceCollectionExtensiosn

---
+++ src/BaseApiStuff/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BaseApiStuff.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BaseApiStuff.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAuth<TUser>(this IServiceCollection serviceCollection) where TUser : IdentityUser
+    {
+        serviceCollection.AddScoped<IAuthService, BaseAuthService<TUser>>();
+        
+        return serviceCollection;
+    }
+}
